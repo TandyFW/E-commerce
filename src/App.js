c3ed60a309385a3501637cc1eb0308c45ecd413e
Tandy <tandyfw@gmail.com>
2021-03-29 10:19:10 -0300

    bootstrap configuraçoes iniciais, Header

--- src/App.js
+++ src/App.js
@@ -5,16 +5,30 @@ import Products from './pages/Products';
 import Services from './pages/Services';
 import Clients from './pages/Clients';
 import NewProduct from './pages/NewProducts';
+import { Helmet } from 'react-helmet';
 
 function App() {
   return (
-    <Switch>
-      <Route exact path="/" component={ Home } />
-      <Route path="/products" component={ Products } />
-      <Route path="/addProdutcs" component={ NewProduct } />
-      <Route path="/services" component={ Services } />
-      <Route path="/clients" component={ Clients } />
-    </Switch>
+    <>
+      <Helmet>
+        <meta name="viewport" content="width=device-width" />
+        <meta charset="utf-8" />
+        <title>Fenix</title>
+        <link
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css"
+          rel="stylesheet"
+          integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6"
+          crossorigin="anonymous"
+        />
+      </Helmet>
+      <Switch>
+        <Route exact path="/" component={ Home } />
+        <Route path="/products" component={ Products } />
+        <Route path="/addProdutcs" component={ NewProduct } />
+        <Route path="/services" component={ Services } />
+        <Route path="/clients" component={ Clients } />
+      </Switch>
+    </>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,12 +3,12 @@ import { Link } from "react-router-dom";
 function Header() {
   return(
     <header className="header">
-      <nav className="page-links">
-        <ul>
-          <li><Link to="/">Página Inicial</Link></li>
-          <li><Link to="/products">Produtos</Link></li>
-          <li><Link to="/services">Serviços</Link></li>
-          <li><Link to="clients">Clientes</Link></li>
+      <nav className="navbar navbar-light bg-light">
+        <ul className="nav navbar">
+          <li className="nav-item"><Link to="/" className="nav-link" aria-disabled="false">Página Inicial</Link></li>
+          <li className="nav-item"><Link to="/products" className="nav-link" aria-disabled="false">Produtos</Link></li>
+          <li className="nav-item"><Link to="/services" className="nav-link" aria-disabled="false">Serviços</Link></li>
+          <li className="nav-item"><Link to="clients" className="nav-link" aria-disabled="false">Clientes</Link></li>
         </ul>
       </nav>
     </header>
